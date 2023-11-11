8a447c3fed8e751ec032e830e9692c7376b94a00
jsrasheduzzaman <jsrasheduzzaman02@gmail.com>
2023-11-11 04:06:28 -0500

    settings to replace special characters

--- unity-users-birthday-email.php
+++ unity-users-birthday-email.php
@@ -36,10 +36,8 @@ class Unity_Birthday {
         add_action( 'admin_enqueue_scripts', [ $this, 'unity_admin_scripts' ] );
         add_action( 'wp', [$this, 'event_trigger_schedule'] );
         add_filter( 'plugin_action_links_' . plugin_basename(__FILE__), [$this, 'plugin_action_links'] );
-
-
         // add_action( 'unity_daily_event', [$this, 'unity_mail_function'] );
-        add_action( 'init',[$this, 'unity_mail_function'] );
+        add_action( 'init', [$this, 'unity_mail_function'] );
     }
 
 
@@ -86,7 +84,14 @@ class Unity_Birthday {
 
     public function unity_mail_function() {
 
-        // if (gmdate('G') !== "8") {
+        $options = get_option( 'unity_birthday_setting' );
+        if (is_array($options)) {
+            $sendTime = isset($options['unity_set_email_time']) ? $options['unity_set_email_time'] : 0;
+        }else{
+            $sendTime = 0;
+        }
+
+        // if (gmdate('G') !== $sendTime) {
         //     return;
         // }
 
@@ -128,31 +133,21 @@ class Unity_Birthday {
 
                     if ($todayDay == $birthday && $todayMonth == $birthmonth) {
 
-                        $username = '';
-                        if($user->get('first_name')){
-                            $username = $user->get('first_name');
-                        }elseif($user->get('nickname')){
-                            $username = $user->get('nickname');
-                        }else{
-                            $username = $user->get('display_name');
-                        }
-
 
                         $userName   = $user->get('user_login');
                         $fName      = $user->get('first_name') ?? $user->get('user_login');
                         $lname      = $user->get('last_name') ?? $user->get('user_login');
                         $niceName   = $user->get('user_nicename') ?? $user->get('user_login');
-                        $dName      = $user->get('display_name') ?? $user->get('user_login');
+                        $disName    = $user->get('display_name') ?? $user->get('user_login');
                         $fullName   = $user->get('first_name') && $user->get('first_name') ? $fName .' '. $lname : $user->get('user_login');
 
 
-                        $options = get_option( 'unity_birthday_setting' );
                         if (is_array($options)) {
                             $sendTime = isset($options['unity_set_email_time']) ? $options['unity_set_email_time'] : 0;
                             $fromName = isset($options['unity_set_from_name']) ? $options['unity_set_from_name'] : get_bloginfo('name');
                             $fromEmail = isset($options['unity_set_from_email']) ? $options['unity_set_from_email'] : get_bloginfo('admin_email');
                             $mailSub = isset($options['unity_email_temp_sub']) ? $options['unity_email_temp_sub'] : 'Happy Birthday @username';
-                            $mailDesc = isset($options['unity_email_temp_desc']) ? $options['unity_email_temp_desc'] : '<h2>Happy Birthday @firstname@</h2> <img src="' . plugin_dir_url( dirname( __FILE__ ) ) . 'images/unity-birthday-email.jpg">';
+                            $mailDesc = isset($options['unity_email_temp_desc']) ? $options['unity_email_temp_desc'] : '<h2>Happy Birthday @firstname@</h2> <img src="' . plugin_dir_url( dirname( __FILE__ ) ) . 'unity-users-birthday-email/images/unity-birthday-email.jpg">';
                             $checked = isset($options['unity_set_notification_too']) ? true : false;
                             $notify2Email = isset($options['unity_set_notify_too_email']) ? $options['unity_set_notify_too_email'] : get_bloginfo('admin_email');
                         }else{
@@ -160,13 +155,36 @@ class Unity_Birthday {
                             $fromName = get_bloginfo('name');
                             $fromEmail = get_bloginfo('admin_email');
                             $mailSub = 'Happy Birthday @firstname@';
-                            $mailDesc = '<h2>Happy Birthday @firstname@</h2> <img src="' . plugin_dir_url( dirname( __FILE__ ) ) . 'images/unity-birthday-email.jpg">';
+                            $mailDesc = '<h2>Happy Birthday @firstname@</h2> <img src="' . plugin_dir_url( dirname( __FILE__ ) ) . 'unity-users-birthday-email/images/unity-birthday-email.jpg">';
                             $checked = false;
                             $notify2Email = get_bloginfo('admin_email');
                         }
 
 
+                        // Replace
+                        $fromName   =   str_replace("@username@",$userName,$fromName);
+                        $mailSub    =   str_replace("@username@",$userName,$mailSub);
+                        $mailDesc   =   str_replace("@username@",$userName,$mailDesc);
 
+                        $fromName   =   str_replace("@fullname@",$fullName,$fromName);
+                        $mailSub    =   str_replace("@fullname@",$fullName,$mailSub);
+                        $mailDesc   =   str_replace("@fullname@",$fullName,$mailDesc);
+
+                        $fromName   =   str_replace("@firstname@",$fName,$fromName);
+                        $mailSub    =   str_replace("@firstname@",$fName,$mailSub);
+                        $mailDesc   =   str_replace("@firstname@",$fName,$mailDesc);
+
+                        $fromName   =   str_replace("@lastname@",$lname,$fromName);
+                        $mailSub    =   str_replace("@lastname@",$lname,$mailSub);
+                        $mailDesc   =   str_replace("@lastname@",$lname,$mailDesc);
+
+                        $fromName   =   str_replace("@nickname@",$niceName,$fromName);
+                        $mailSub    =   str_replace("@nickname@",$niceName,$mailSub);
+                        $mailDesc   =   str_replace("@nickname@",$niceName,$mailDesc);
+
+                        $fromName   =   str_replace("@displayname@",$disName,$fromName);
+                        $mailSub    =   str_replace("@displayname@",$disName,$mailSub);
+                        $mailDesc   =   str_replace("@displayname@",$disName,$mailDesc);
 
 
                         $to = $user->get('user_email');
@@ -184,9 +202,9 @@ class Unity_Birthday {
                             </body>
                             </html>
                             ';
-                        // if(wp_mail( $to,$subject,$message,$headers ) && $checked ){
-                        //     wp_mail($notify2Email, 'Notification of Birthday Email Sent', 'A birthday email was sent to ' . $username . '.', $headers);
-                        // }
+                        if( wp_mail( $to,$subject,$message,$headers ) && $checked ){
+                            wp_mail($notify2Email, 'Notification of Birthday Email Sent', 'A birthday email was sent to ' . $fullName . '.', $headers);
+                        }
                     }
                 }
             }
